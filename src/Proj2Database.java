08da88ada5094fed532fe1f1073be30074ddebae
Adam Bellmyer <adambellmyer@gmail.com>
2022-04-28 11:49:35 -0700

    Implementing Orders table for Req#3
    Added the orders table so I can finish implementing Req #3

--- src/Proj2Database.java
+++ src/Proj2Database.java
@@ -44,12 +44,17 @@ public class Proj2Database {
 //        		+ "  FOREIGN KEY (item) REFERENCES Item(upc)\r\n"
 //        		+ "  FOREIGN KEY (department) REFERENCES Item(department)\r\n"
 //        		+ ");";
-//        String query = "CREATE TABLE IF NOT EXISTS Orders(\r\n"
-//        		+ "  id INTEGER NOT NULL,\r\n"
-//        		+ "  itemOrdered INTEGER NOT NULL,\r\n"
-//        		+ "  PRIMARY KEY (id),\r\n"
-//        		+ "  FOREIGN KEY (itemOrdered) REFERENCES Item(upc)\r\n"
-//        		+ ");";
+        String query = "CREATE TABLE IF NOT EXISTS Orders("
+        		+ "  id INTEGER NOT NULL,\r\n"
+        		+ "  itemOrdered INTEGER NOT NULL,\r\n"
+        		+ "  qty INTEGER NOT NULL,\r\n"
+        		+ "  orderDate TEXT NOT NULL,\r\n"
+        		+ "  onDelivery NUMERIC NOT NULL,\r\n"
+        		+ "  delivery INTEGER,\r\n"
+        		+ "  PRIMARY KEY (id),\r\n"
+        		+ "  FOREIGN KEY (itemOrdered) REFERENCES Item(upc),\r\n"
+        		+ "  FOREIGN KEY (delivery) REFERENCES Delivery(id)\r\n"
+        		+ ");";
 //        String query = "CREATE TABLE IF NOT EXISTS Delivery(\r\n"
 //        		+ "  id INTEGER NOT NULL,\r\n"
 //        		+ "  arrivalDate TEXT NOT NULL,\r\n"
@@ -58,24 +63,21 @@ public class Proj2Database {
 //        		+ "  PRIMARY KEY (id)\r\n"
 //        		+ ");";
         
-        //String insertQuery = "INSERT INTO Orders VALUES(10, 4798);";
+        String insertQuery = "INSERT INTO Orders VALUES(10, 4798, 32, '2022-01-14', TRUE, 2341);";
         //String insertQuery = "INSERT INTO Delivery VALUES(2341, '2022-01-17', 2, 14);";
         //stmt.dropQuery = "DROP TABLE IF EXISTS Orders;";
 //        String insertQuery2 = "INSERT INTO ExpirationDates VALUES('2022-04-26', 3972, 1);";
 //        String insertQuery3 = "INSERT INTO ExpirationDates VALUES('2022-04-28', 4912, 3);";
-        String selectQuery = "SELECT * FROM Delivery;";
+        String selectQuery = "SELECT * FROM Orders;";
         
         
         try ( Connection conn = ds.getConnection();
               Statement stmt = conn.createStatement(); ) {
-//        		stmt.executeUpdate(query);
-//        		stmt.executeUpdate(insertQuery);
+        		stmt.executeUpdate(query);
+        		//stmt.executeUpdate(insertQuery);
         	ResultSet res = stmt.executeQuery(selectQuery);
         	while(res.next()) {
         		System.out.println(res.getInt("id"));
-        		System.out.println(res.getString("arrivalDate"));
-        		System.out.println(res.getInt("numOfPallets"));
-        		System.out.println(res.getInt("truckID"));
         	}
         	
         	
